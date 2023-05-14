90e395475e80efd19f7248d226323a711185fae7
John Sloan <jsloanphoto@gmail.com>
2023-05-14 12:38:00 -0400

    added Route class, need to try and figure out how to make the Route stand alone so the climber only interacts with that object.

--- gamedemo/climbers.py
+++ gamedemo/climbers.py
@@ -27,6 +27,30 @@ class Climber:
         return self.health > 0
     
     def __str__(self):
-        return "Player {} has {} health and bring his {} to the fight".format(
+        return "{} has {} health and bring his {} to the fight".format(
             self.name, self.health, type(self.weapon).__name__
+        )
+    
+class Route:
+    """
+    The Route class represents the route the climber is trying to climb
+    """
+
+    def __init__(self, name, weapon):
+        self.name = name
+        self.weapon = weapon
+        self.health = 100
+
+    def take_hit(self, damage):
+        self.health -= damage
+        return self.health
+    
+    @property
+    def unsent(self):
+        """True if :attr: 'health' is larger than 0, False otherwise"""
+        return self.health > 0
+    
+    def __str__(self):
+        return "{} is still standing with {} left to climb".format(
+            self.name, self.health
         )
--- gamedemo/game.py
+++ gamedemo/game.py
@@ -12,8 +12,8 @@ __author__ = "John Christopher Sloan"
 
 import random
 
-from skills import Attempt, Flow
-from climbers import Climber
+from skills import Attempt
+from climbers import Climber, Route
 
 class Game:
     def __init__(self, player1, player2):
@@ -24,7 +24,7 @@ class Game:
         print('Welcome to the Red River Gorge Climbing Game')
         print(self.p1)
         print(self.p2)
-        while self.p1.is_alive and self.p2.is_alive:
+        while self.p1.is_alive and self.p2.unsent:
             if random.choice([True, False]):
                 attacker = self.p1
                 defender = self.p2
@@ -32,7 +32,7 @@ class Game:
                 attacker = self.p2
                 defender = self.p1
             dmg, sound = attacker.weapon.redpoint_attempt()
-            print(attacker.name, 'attacks:', sound)
+            print(attacker.name, 'tries to', sound)
             print(attacker.name, 'did', dmg, 'damage')
             defender.take_hit(dmg)
         print(attacker.name, 'won with', attacker.health, 'health left')
@@ -40,5 +40,5 @@ class Game:
 
 if __name__ == '__main__':
     random.seed()
-    g = Game(Climber('Climber', Attempt()), Climber('Route', Attempt()))
+    g = Game(Climber('Climber', Attempt()), Route('Route', Attempt()))
     g.run()
